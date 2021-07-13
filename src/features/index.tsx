400f18c181ea4526ec16dd59807269e1ad8da206
jaeyoung <66120479+kwakjy@users.noreply.github.com>
2021-07-13 15:22:08 +0900

    TodoItem 인터페이스, createSlice, createEntityAdapter 구현
    interface를 사용하여 TodoItem의 요소들의 타입을 지정하였고, createSlice를 사용해 action과 reducer를 정의할 틀을 만들었고, 후에 더 효율적인 배열 사용을 위해 createEntityAdapter와 EntityState 또한 구현해주었다.

--- src/features/index.tsx
+++ src/features/index.tsx
@@ -1,5 +1,31 @@
-import {combineReducers} from '@reduxjs/toolkit'
+import {combineReducers, createSlice, createEntityAdapter, EntityState} from '@reduxjs/toolkit'
+
+export interface TodoItem {
+  id: number,
+  content: string,
+  check: boolean,
+}
+
+interface TodoItemList {
+  list: EntityState<TodoItem>,
+}
+
+const todoAdapter = createEntityAdapter<TodoItem>({
+  selectId: (item) => item.id,
+})
+
+const initialState: TodoItemList = {
+  list: todoAdapter.getInitialState(),
+}
+
+const todoSlice = createSlice({
+  name: 'TodoItem',
+  initialState,
+  reducers: {
+
+  }
+})
 
 export const rootReducer = combineReducers({
-
+  todos: todoSlice.reducer
 })
